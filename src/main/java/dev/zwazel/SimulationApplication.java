d4aa09783336b1cc53cf52a02cd5b15b12117747
Tim Irmler <zwazel@hotmail.com>
2025-01-21 18:34:55 +0100

    mm

--- src/main/java/dev/zwazel/SimulationApplication.java
+++ src/main/java/dev/zwazel/SimulationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimulationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimulationApplication.class, args);
     }
