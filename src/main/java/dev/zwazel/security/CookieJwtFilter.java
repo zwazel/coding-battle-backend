5d334c61b0592ed1da2db338ca44767fed8deea7
Tim Irmler <42088586+zwazel@users.noreply.github.com>
2025-06-17 16:38:11 +0200

    Refactor CookieJwtFilter to run chain once

--- src/main/java/dev/zwazel/security/CookieJwtFilter.java
+++ src/main/java/dev/zwazel/security/CookieJwtFilter.java
@@ -24,22 +24,22 @@ class CookieJwtFilter implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange ex, WebFilterChain chain) {
         log.debug("Starting filter for request: {}", ex.getRequest().getURI());
-        Mono<Void> next = chain.filter(ex);
         return Mono.justOrEmpty(ex.getRequest().getCookies().getFirst(jwtCookieName))
                 .doOnNext(c -> log.debug("JWT cookie found: {}", jwtCookieName))
                 .flatMap(c -> jwtUtil.validate(c.getValue())
                         .doOnSuccess(auth -> log.debug("JWT successfully validated"))
                         .doOnError(e -> log.warn("Error during JWT validation: {}", e.getMessage()))
-                        .flatMap(auth -> {
-                            log.debug("Setting authentication in security context");
-                            return next.contextWrite(
-                                    ReactiveSecurityContextHolder.withAuthentication(auth));
-                        }))
-                .then(next) // ensure we return the next filter in the chain
-                .onErrorResume(e -> {
-                    log.error("JWT validation failed: {}", e.getMessage());
-                    return next; // continue chain even on error
-                });
+                        .onErrorResume(e -> {
+                            log.error("JWT validation failed: {}", e.getMessage());
+                            return Mono.empty();
+                        })
+                )
+                .flatMap(auth -> {
+                    log.debug("Setting authentication in security context");
+                    return chain.filter(ex)
+                            .contextWrite(ReactiveSecurityContextHolder.withAuthentication(auth));
+                })
+                .switchIfEmpty(chain.filter(ex));
     }
 
 }
